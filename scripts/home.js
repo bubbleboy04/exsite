87d7801ebcbeb0ec74138b80de85415ddafe2d02
bubbleboy04 <96508239+bubbleboy04@users.noreply.github.com>
2021-12-24 02:33:33 -0600

    updated site changes

--- scripts/home.js
+++ scripts/home.js
@@ -1,35 +1,19 @@
-console.log("scriptloaded");
-function footballButton() {
-    document.getElementById("info").style.display = "block";
-    document.getElementById("home").style.display = "none";
-    document.getElementById("gameTitle").textContent = "Football QB";
-}
+var mainimage = document.getElementById("mainImage");
+var home = document.getElementById("home");
+var tx = document.getElementById("gameTitle");
+
+
+
 function imageClick(_elem) {
-    document.getElementById("mainImage").src = _elem.src;
-    var t = document.getElementById("imageDesc")
-    console.log(_elem.id);
-    switch (_elem) {
-        case sideImage0:
-            console.log("case hit");
-            t.textContent = "First Stage of Development";
-            break;
-        case sideImage1:
-            console.log("case hit");
-            t.textContent = "Second Stage of Development";
-            break;
-        case sideImage2:
-            console.log("case hit");
-            t.textContent = "Third Stage of Development";
-            break;
-        case sideImage3:
-            console.log("case hit");
-            t.textContent = "Forth Stage of Development";
-            break;
-        case sideImage4:
-            console.log("case hit");
-            t.textContent = "Fifth Stage of Development";
-            break;
-        default:
-            break;
-    }
+    mainimage.src = _elem.src;
 }
+function spookyClick() {
+    footballContainer.style.display = "none";
+    ghostContainer.style.display = "block";
+    home.style.display = "none";
+    tx.textContent = "Poltergeist";
+}
+function homeButton() {
+    
+}
+
